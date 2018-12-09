593d2f5dabdda6407b6bdf2afc8902c4ba1b3236
applecrumble123 <30610249+applecrumble123@users.noreply.github.com>
2018-12-10 01:11:05 +0800

    student profile
    added the slider function

--- Frontend/JavaScript.js
+++ Frontend/JavaScript.js
@@ -6,3 +6,10 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
